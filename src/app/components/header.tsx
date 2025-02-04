29385652be47159af2d6adcd8538cc0e42602171
Charles Amen <charlesamenlokonon@gmail.com>
2025-02-04 17:42:56 +0100

    First commit

---
+++ src/app/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="text-center mx-auto w-full">
+            Private Docs
+        </div>
+    );
+};
+
+export default Header;
